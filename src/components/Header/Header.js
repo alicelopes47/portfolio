2dcfe72bc8436fbe2090d1871bc8ef29ba5acc4b
alicelopes47 <lopesalice4745@gmail.com>
2023-04-17 10:19:13 -0300

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from "styled-components"
+import * as S from './Header_style'
+
+const Header = () => {
+  return (
+    <>
+    <S.HeaderNav> 
+    <img src='' alt='' />
+    <ul>
+      <li>Oii</li>
+      <li>Linda</li>
+      <li>Perfeita</li>
+    </ul>
+    </S.HeaderNav> 
+    
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/Header/Header_style.js
@@ -0,0 +1,5 @@
+import styled from "styled-components"
+
+export const HeaderNav = styled.nav`
+    border: solid black    
+`
