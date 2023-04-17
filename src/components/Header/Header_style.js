84bf36f259349887c4b8cbf222eb3807dc9d055c
alicelopes47 <lopesalice4745@gmail.com>
2023-04-17 12:29:19 -0300

    working on main

--- src/components/Header/Header_style.js
+++ src/components/Header/Header_style.js
@@ -9,19 +9,23 @@ export const HeaderNav = styled.nav`
 export const HeaderList = styled.ul`
     color: white;
     display: flex;
-    font-size: 1.8em;
+    font-size: 2em;
     gap: 3em;
     list-style: none;
     cursor: pointer;
 `
 export const HeaderItem = styled.li `
+    padding: 0.5em;
+    border-radius: 0.5em;
     :hover { 
-        font-size: 1.3em;
-        transition: 0.1s;
+        font-size: 1.1em;
+        padding: 0.5em;
+        background-color: black;;
+        transition: 0.5s;
     }
 `
 
 export const Logo = styled.img `
     width: 8em;
-    padding: 1em;
+    padding: 0.5em;
 `
