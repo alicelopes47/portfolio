e9659f017d7b13df7fd446b489ed96c032bb57fb
Alice Lopes <lopesalice4745@gmail.com>
2024-06-23 01:31:19 -0300

    chore: Add @lottielab/lottie-player package to dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,38 @@
-'use client'
-import { Kodchasan } from 'next/font/google'
-const kodchasan = Kodchasan({subsets: ['latin'], weight: ['200']})
+"use client";
+import { Kodchasan } from "next/font/google";
+const kodchasan = Kodchasan({ subsets: ["latin"], weight: ["200"] });
+import Lottie from "@lottielab/lottie-player/react";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <main>
-      <div className={`${kodchasan.className} flex flex-col items-center`}>
-        <h1 className={`text-3xl md:text-5xl`}>Olá, meu nome é <span className="text-primaryBold">Alice Lopes</span></h1>
-        <h1 className={`text-3xl text-primaryBold`}>Desenvolvedora web.</h1>
+      <div
+        className={`${kodchasan.className} flex px-2 flex-col items-center md:mt-14 mt-8 gap-12`}
+      >
+        <motion.h1
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5 }}
+          className={`md:text-[3rem] text-3xl`}
+        >
+          Olá, meu nome é <span className="text-primaryBold">Alice Lopes</span>
+        </motion.h1>
+        <Lottie
+          src="https://cdn.lottielab.com/l/3KEkh3sM9Tnhxu.json"
+          autoplay
+          className="md:w-[25%] w-[55%]"
+        />
+        <motion.h1
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          className={`md:text-[3rem] text-3xl text-primaryBold`}
+        >
+          Desenvolvedora web.
+        </motion.h1>
       </div>
     </main>
-  )
+  );
 }
