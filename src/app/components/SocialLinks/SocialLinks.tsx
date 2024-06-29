c37b65c9fa1e5d7df9f9c958ed402f56ff848344
Alice Lopes <lopesalice4745@gmail.com>
2024-06-28 21:18:13 -0300

    chore: Update SocialLinks component with new icons

--- src/app/components/SocialLinks/SocialLinks.tsx
+++ src/app/components/SocialLinks/SocialLinks.tsx
@@ -1,18 +1,16 @@
-import { BsFillFileEarmarkPersonFill } from "react-icons/bs";
-import { CiLinkedin } from "react-icons/ci";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { LiaGithub, LiaLinkedin, LiaFileArchive } from "react-icons/lia";
 
 export function SocialLinks() {
     return (
-        <div data-aos="fade-up" className="flex flex-col z-50 md:p-4 p-2 gap-2 rounded-md md:gap-14 bg-primary fixed md:bottom-4 bottom-0 md:left-5 left-0">
+        <div data-aos="fade-up" className="flex rounded-md   flex-col z-50 md:p-4 p-2 gap-4 bg-[#c946f117] fixed md:bottom-4 bottom-0 md:left-5 left-0">
             <a href="https://www.linkedin.com/in/alice-lopes-%F0%9F%8F%B3%EF%B8%8F%E2%80%8D%E2%9A%A7%EF%B8%8F-423a1a249/" target="_blank" rel="noreferrer">
-                <FaLinkedin className="md:text-4xl text-2xl text-white" />
+                <LiaLinkedin className="md:text-4xl text-3xl text-primary" />
             </a>
             <a href="https://github.com/alicelopes47" target="_blank" rel="noreferrer">
-                <FaGithub className="md:text-4xl text-2xl text-white" />
+                <LiaGithub className="md:text-4xl text-3xl text-primary" />
             </a>
             <a href="https://www.linkedin.com/in/rafael-fernandes-7b7b7b1b4/" target="_blank" rel="noreferrer">
-                <BsFillFileEarmarkPersonFill className="md:text-4xl text-2xl text-white" />
+                <LiaFileArchive className="md:text-4xl text-3xl text-primary" />
             </a>
         </div>
     )
