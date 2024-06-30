ab30b205e06c7c6f3bff7ad12a7b9a6798e7e41b
Alice Lopes <lopesalice4745@gmail.com>
2024-06-30 20:53:44 -0300

    add favicon

--- _todos.js
+++ _todos.js
@@ -1,4 +1,2 @@
-// TODO: ajustar layout.tsx para ser um server side render
 // TODO: mudar nome da aba do site
-// TODO: mudar favicon
 // TODO: adicionar um loading geral
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={`bg-hexagonPattern`}>
         {children}
       </body>
