a43186329aba554912dfe946a5421498da0bd938
Alice Lopes <lopesalice4745@gmail.com>
2024-06-30 11:45:02 -0300

    chore: Update SocialLinks component with new icons

--- src/app/components/SocialLinks/SocialLinks.tsx
+++ src/app/components/SocialLinks/SocialLinks.tsx
@@ -1,4 +1,5 @@
 import { LiaGithub, LiaLinkedin, LiaFileArchive } from "react-icons/lia"
+import { LinkWithAlert } from "../LinkWithAlert/LinkWithAlert"
 
 export function SocialLinks() {
 	return (
@@ -6,23 +7,21 @@ export function SocialLinks() {
 			data-aos="fade-up"
 			className="flex rounded-md   flex-col z-50 md:p-4 p-2 gap-4 bg-[#c946f117] fixed md:bottom-4 bottom-0 md:left-5 left-0"
 		>
-			<a
+			<LinkWithAlert
 				href="https://www.linkedin.com/in/alice-lopes-%F0%9F%8F%B3%EF%B8%8F%E2%80%8D%E2%9A%A7%EF%B8%8F-423a1a249/"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<LiaLinkedin className="md:text-4xl text-3xl text-primary" />
-			</a>
-			<a href="https://github.com/alicelopes47" target="_blank" rel="noreferrer">
-				<LiaGithub className="md:text-4xl text-3xl text-primary" />
-			</a>
-			<a
-				href="https://www.linkedin.com/in/rafael-fernandes-7b7b7b1b4/"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<LiaFileArchive className="md:text-4xl text-3xl text-primary" />
-			</a>
+				icon={<LiaLinkedin className="md:text-4xl text-3xl text-primary" />}
+				message="Linkedin"
+			/>
+			<LinkWithAlert
+				href="https://github.com/alicelopes47"
+				icon={<LiaGithub className="md:text-4xl text-3xl text-primary" />}
+				message="Github"
+			/>
+			<LinkWithAlert
+				href="https://github.com/alicelopes47"
+				icon={<LiaFileArchive className="md:text-4xl text-3xl text-primary" />}
+				message="Curriculo"
+			/>
 		</div>
 	)
 }
