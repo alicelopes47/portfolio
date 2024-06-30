ad9eb404a8c43a0edfd5a1602a61d35dafb1359b
Alice Lopes <lopesalice4745@gmail.com>
2024-06-30 13:17:02 -0300

    refactor: Improve SkillsPage component structure and styling

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -10,38 +10,46 @@ interface Props {
 export default function SkillsPage({ skills }: Props) {
 	return (
 		<div className="md:px-24 px-12 scroll-mt-[10rem]" id={LinksPT.SKILLS}>
-			<h1
-				className={`md:text-[2.5rem] text-3xl text-primaryBold md:text-start text-center  mb-8`}
-			>
+			<h1 className={`md:text-[2.5rem] text-3xl text-primaryBold md:text-start text-center  md:mb-12 mb-8`}>
 				habilidades
 			</h1>
-				<div className="mt-5 grid gap-5 grid-cols-1 md:grid-cols-3 w-full">
-					{skills.map((skill, index) => (
-						<>
-							<Card title={skill.title} icon={<LiaReact className="text-6xl text-primary" />} description={skill.description} key={skill.title}>
-								<Meteors indx={index} number={5} />
-							</Card>
-						</>
-					))}
-				</div>
+			<div className="mt-5 grid gap-5 grid-cols-1 md:grid-cols-3 w-full">
+				{skills.map((skill, index) => (
+					<>
+						<Card
+							title={skill.title}
+							icon={<LiaReact className="text-6xl text-primary" />}
+							description={skill.description}
+							key={skill.title}
+						>
+							<Meteors indx={index} number={5} />
+						</Card>
+					</>
+				))}
+			</div>
 		</div>
 	)
 }
 
 export interface CardProps {
-    title: string;
-    description: string;
-    icon: React.ReactNode;
-    children: React.ReactNode;
+	title: string
+	description: string
+	icon: React.ReactNode
+	children: React.ReactNode
 }
 
-const Card = ({title, description, icon, children} : CardProps) => {
+const Card = ({ title, description, icon, children }: CardProps) => {
 	return (
-		<div data-aos="fade-up" className="rounded-2xl flex flex-col items-start p-4 cursor-pointer hover:bg-primaryLight transition duration-200h-full w-full overflow-hidden bg-white border border-primary">
-                <div className="flex justify-center p-4">{icon}</div>
-				<div>{title}</div>
-                <h1>{description}</h1>
-                {children}
+		<div className="transition-all hover:animate-pulse hover:bg-[#f6effa] rounded-2xl hover:scale-105">
+			<div
+				data-aos="fade-up"
+				className="rounded-2xl flex flex-col items-start p-4 h-[100%] cursor-pointer hover:bg-primaryLight transition duration-200h-full overflow-hidden bg-white border border-primary"
+			>
+				<div className="flex justify-center p-4">{icon}</div>
+				<div className="font-bold">{title}</div>
+				<h1>{description}</h1>
+				{children}
+			</div>
 		</div>
 	)
 }
