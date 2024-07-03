8ef32b7c73baaff289ed97013a45917f6aabe78a
Alice Lopes <lopesalice4745@gmail.com>
2024-07-02 23:05:40 -0300

    refactor: Update DropDown component to use single quotes for imports and add hrefsArray for link hrefs

--- src/app/components/DropDown.tsx
+++ src/app/components/DropDown.tsx
@@ -1,6 +1,7 @@
-import Link from "next/link"
-import { useEffect, useRef, useState } from "react"
-import { FaBars } from "react-icons/fa"
+import Link from 'next/link'
+import { useEffect, useRef, useState } from 'react'
+import { FaBars } from 'react-icons/fa'
+import { hrefsArray } from '../Domain'
 
 interface Props {
 	links: string[]
@@ -18,28 +19,36 @@ const DropDown = ({ links, activeSection }: Props) => {
 	useEffect(() => {
 		function handleClickOutside(event: { target: any }) {
 			if (navbarRef.current && !navbarRef.current.contains(event.target)) {
-				setIsOpen(false);
+				setIsOpen(false)
 			}
 		}
-		document.addEventListener("mousedown", handleClickOutside);
+		document.addEventListener('mousedown', handleClickOutside)
 		return () => {
-			document.removeEventListener("mousedown", handleClickOutside);
-		};
-	}, [navbarRef]);
+			document.removeEventListener('mousedown', handleClickOutside)
+		}
+	}, [navbarRef])
 
 	return (
 		<>
-			<div className="fixed bg-primary w-full z-50 top-0 p-4 cursor-pointer" onClick={() => setIsOpen(true)}>
+			<div
+				className="fixed bg-primary w-full z-50 top-0 p-4 cursor-pointer"
+				onClick={() => setIsOpen(true)}
+			>
 				<FaBars className="text-2xl text-lightText" />
 			</div>
 			{isOpen && (
-				<div ref={navbarRef} data-aos="fade-right" className="bg-primary flex fixed top-0 z-50 h-full flex-col w-[40%] justify-start pt-[20%] items-start">
+				<div
+					ref={navbarRef}
+					data-aos="fade-right"
+					className="bg-primary flex fixed top-0 z-50 h-full flex-col w-[40%] justify-start pt-[20%] items-start"
+				>
 					{links.map((link, index) => (
 						<Link
-							key={index}
-                            onClick={handleClose}
-							href={`#${link}`}
-							className={`text-start ml-7 text-xl pt-12 hover:text-lightText ${activeSection == link ? " text-lightText" : ""}`}
+							href={`#${hrefsArray[index]}`}
+							onClick={handleClose}
+							className={`text-start ml-7 text-xl pt-12 hover:text-lightText ${
+								activeSection == hrefsArray[index] ? ' text-lightText' : ''
+							}`}
 						>
 							{link}
 						</Link>
