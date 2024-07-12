27d07e6b63b8ab842d06ec8563683b57d8c974be
Alice Lopes <lopesalice4745@gmail.com>
2024-07-12 10:59:55 -0300

    refactor: Update LinkedIn profile link in SocialLinks component

--- src/app/components/SocialLinks.tsx
+++ src/app/components/SocialLinks.tsx
@@ -8,7 +8,7 @@ export function SocialLinks() {
 			className="flex rounded-md flex-col z-50 md:p-4 p-2 gap-4 bg-[#c946f117] fixed md:bottom-4 bottom-0 md:left-5 left-0"
 		>
 			<LinkWithAlert
-				href="https://www.linkedin.com/in/alice-lopes-%F0%9F%8F%B3%EF%B8%8F%E2%80%8D%E2%9A%A7%EF%B8%8F-423a1a249/"
+				href="https://www.linkedin.com/in/alice-lopes-423a1a249/"
 				icon={<LiaLinkedin className="md:text-4xl text-3xl text-icons" />}
 				message="Linkedin"
 			/>
